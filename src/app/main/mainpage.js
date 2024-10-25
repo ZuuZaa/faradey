8bdb929b4fea1fd3ac6be93267cc4df2433fd48a
ZuuZaa <zumbaghirova@gmail.com>
2024-10-25 12:44:25 +0400

    chore: check image onerror

--- src/app/main/mainpage.js
+++ src/app/main/mainpage.js
@@ -1057,7 +1057,8 @@ const MainPage = () => {
                           src={catimg.image}
                           width={300}
                           height={400}
-                          onError={(e) => (e.target.src = noImage)}
+                          onError={(e) => console.error(e.target.src)}
+                          //onError={(e) => (e.target.src = noImage)}
                           className="blog-swiper-item-img object-cover mb-3"
                           alt={catimg.name}
                         />
