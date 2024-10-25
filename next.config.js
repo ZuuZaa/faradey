7f7512fdb9cf36c4997cd65b9d8cf76c34769f39
ZuuZaa <zumbaghirova@gmail.com>
2024-10-25 12:06:21 +0400

    fix: config for images

--- next.config.js
+++ next.config.js
@@ -2,19 +2,15 @@
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    domains: ["faradey.logixvps.cloud", "faradeyapi.logixcommerce.com"],
-  },
-  // images: {
-  //   remotePatterns: [
-  //     {
-  //       protocol: "http",
-  //       hostname: "faradey.logixvps.cloud",
-  //       port: "3460",
-  //       pathname: "/assets/uploads/**",
-  //     },
-  //   ],
-  //  " http://faradey.logixvps.cloud:3460//assets/uploads/team/1.webp"
-  // },
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "faradey.logixvps.cloud",
+        port: "3460",
+        pathname: "/assets/uploads/**",
+      },
+    ],
+ },
 };
 
 module.exports = nextConfig;
