edadec3466ef8b4620cce39467f141b396136cc6
ZuuZaa <zumbaghirova@gmail.com>
2024-11-11 11:23:53 +0400

    feat: add change language func for info page

--- src/app/info/[kind]/page.js
+++ src/app/info/[kind]/page.js
@@ -7,7 +7,7 @@ import { useTranslation } from "react-i18next";
 
 const InfoPage = () => {
   const [pageContent, setPageContent] = useState(null);
-  const { i18n } = useTranslation();
+  const { t, i18n } = useTranslation();
   const params = useParams();
   let lang_id = "EN";
   if (
@@ -33,14 +33,39 @@ const InfoPage = () => {
 
       if (res.status === 200) {
         const resJson = await res.json();
-
-        console.log("data", resJson.output);
         return resJson.output;
       }
     } catch (err) {
       console.log(err);
     }
   };
+
+  const changeLanguage = async (event) => {
+    const lng = event.currentTarget.textContent;
+    await i18n.changeLanguage(lng);
+    if (typeof localStorage !== "undefined") {
+      localStorage.setItem("langId", lng);
+    }
+    const fetchedData = await getPageData();
+    setPageContent(fetchedData);
+  };
+
+  useEffect(() => {
+    // Get all elements with the class name 'lang_btn'
+    const langBtns = document.getElementsByClassName("lang_btn");
+
+    // Add click event listener to each button
+    for (let i = 0; i < langBtns.length; i++) {
+      langBtns[i].addEventListener("click", changeLanguage);
+    }
+
+    // Cleanup function to remove event listeners when the component unmounts
+    return () => {
+      for (let i = 0; i < langBtns.length; i++) {
+        langBtns[i].removeEventListener("click", changeLanguage);
+      }
+    };
+  }, []);
 
   useEffect(() => {
     async function fetchDataAsync() {
@@ -49,7 +74,7 @@ const InfoPage = () => {
       await i18n.changeLanguage(lang_id);
     }
     fetchDataAsync();
-  }, []);
+  }, [lang_id]);
 
   return (
     <div className="flex justify-center items-center py-8">
